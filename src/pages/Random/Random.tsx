4120bd37b83f281c39a72ff3746306abec7407ab
afshan-noor <143619568+afshan-noor@users.noreply.github.com>
2023-09-17 22:45:36 -0700

    change in Random.tsx

--- src/pages/Random/Random.tsx
+++ src/pages/Random/Random.tsx
@@ -1,15 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from "./Random.module.css"
 
-
-
-
-
-
-
-
-
-
 const RandomRecipe:React.FC = () => {
   const [randomRecipe, setRandomRecipe] = useState(null);
 
